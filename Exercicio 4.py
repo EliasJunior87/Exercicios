78f9fd2a8bc8f34722d3534ab4616179e32f703b
Elias Junior <48299934+EliasJunior87@users.noreply.github.com>
2021-09-01 11:51:41 -0300

    Add files via upload

---
+++ Exercicio 4.py
@@ -0,0 +1,8 @@
+# Exercício 4
+# Faça um programa que solicite três números inteiros do usuário e imprima a soma destes.
+
+num1 = int(input('número 1: '))
+num2 = int(input('Número 2: '))
+num3 = int(input('Número 3: '))
+print(f'{num1}+{num2}+{num3}={num1+num2+num3}')
+
