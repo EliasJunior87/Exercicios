98745f250a3ab94741717af06d166ca9ee9abc41
Elias Junior <48299934+EliasJunior87@users.noreply.github.com>
2021-09-01 14:58:47 -0300

    Add files via upload

---
+++ Exercicio 7.py
@@ -0,0 +1,14 @@
+# Exercício 7
+# Faça um programa que peça 5 números de ponto flutuante do
+# usuário e apresente no final a média dos números digitados.
+
+num1 = float(input('Número 1: '))
+num2 = float(input('Número 2: '))
+num3 = float(input('Número 3: '))
+num4 = float(input('Número 4: '))
+num5 = float(input('Número 5: '))
+media=(num1+num2+num3+num4+num5)/5
+print(f'média: {media}')
+
+
+
