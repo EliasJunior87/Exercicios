0328d9f23aa2088b166fc1641742b920d44bf511
Elias Junior <48299934+EliasJunior87@users.noreply.github.com>
2021-09-01 11:37:39 -0300

    Add files via upload

---
+++ Exercicio 2.py
@@ -0,0 +1,18 @@
+# Exercício 2
+# Faça um programa que solicite um número do usuário e apresente a seguinte mensagem na tela:
+# "O número digitado foi [número]".
+
+# Solução 1
+
+num = input('Digite o número: ')
+print(f'O número digitado foi {num}')
+
+# solução 2
+
+num = float(input('Digite um número: '))
+print(f'O numero digitado foi {num} ')
+
+
+
+
+
