6ea00d518d952dae95acb5d73911282ab26d0d65
Elias Junior <48299934+EliasJunior87@users.noreply.github.com>
2021-09-01 15:06:09 -0300

    Add files via upload

---
+++ Exercicio 8.py
@@ -0,0 +1,5 @@
+# Exercício 8
+# Escreva um programa que faça a conversão de um dado valor de metro para quilômetro.
+
+num = float(input('Digite um valor em metros: '))
+print(f'{num} metros equivale a {num/1000} km ')
